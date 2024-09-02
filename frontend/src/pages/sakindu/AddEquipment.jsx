7f5bdf526a91fe0c80e4be129cc2a9a56c5a0998
Sakindu Dinsara <161465821+Loo0per@users.noreply.github.com>
2024-09-03 01:10:29 +0530

    Update AddEquipment.jsx

---
+++ frontend/src/pages/sakindu/AddEquipment.jsx
@@ -0,0 +1,151 @@
+import React, {useState} from 'react';
+import axios from 'axios';
+
+function AddEquipment() {
+
+    const [equipmentId, setPackageId] = useState("");
+    const [equipmentName, setPackage_Title] = useState("");
+    const [equipmentType, setEquipmentType] = useState("");
+    const [equipmentDescription, setPackageDes] = useState("");
+    const [equipmentImage, setEquipmentImage] = useState(null);
+    const [equipmentPrice, setPrice] = useState("");
+    const [equipmentQuantity, setEuipmentQuantity] = useState("");
+
+    function addEquipment(e){
+        e.preventDefault();
+
+        const formData = new FormData();
+        formData.append("equipmentId", equipmentId);
+        formData.append("equipmentName", equipmentName);
+        formData.append("equipmentType", equipmentType);
+        formData.append("equipmentDescription", equipmentDescription);
+        formData.append("equipmentImage", equipmentImage);
+        formData.append("equipmentPrice", equipmentPrice);
+        formData.append("equipmentQuantity", equipmentQuantity);
+        
+        axios.post("http://localhost:5000/Equipment/add", formData).then(() => {
+            alert("Equipment Successfully Added!!");
+        }).catch((err) => {
+            alert(err);
+        });
+
+    }
+
+    function RangeSlider() {
+        const [Qty, setQty] = useState(50);
+      
+        const updateQty = (value) => {
+          setPrice(value);
+        }}
+
+    return (
+        <>
+        <section className="max-w-4xl p-6 mx-auto bg-indigo-600 rounded-md shadow-md dark:bg-gray-800 mt-20">
+          <h1 className="text-xl font-bold text-white capitalize dark:text-white">Add Equipment</h1>
+          <form>
+            <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
+              <div>
+                <label className="text-white dark:text-gray-200" htmlFor="equipmentId">Equipment ID</label>
+                <input
+                  id="equipmentId"
+                  type="text"
+                  className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+                />
+              </div>
+  
+              <div>
+                <label className="text-white dark:text-gray-200" htmlFor="equipmentName">Equipment Name</label>
+                <input
+                  id="equipmentName"
+                  type="text"
+                  className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+                />
+              </div>
+  
+              <div>
+                <label className="text-white dark:text-gray-200" htmlFor="equipmentType">Equipment Type</label>
+                <select
+                  id="equipmentType"
+                  className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+                >
+                  <option>Hiking</option>
+                  <option>Luggage</option>
+                  <option>Clothes</option>
+                  <option>Toiletries</option>
+                </select>
+              </div>
+    
+              <div>
+                <label className="text-white dark:text-gray-200" htmlFor="equipmentDescription">Equipment Description</label>
+                <textarea
+                  id="equipmentDescription"
+                  className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+                />
+              </div>
+
+              <div>
+                <label className="text-white dark:text-gray-200" htmlFor="passwordConfirmation">Equipment Price</label>
+                <input
+                  id="passwordConfirmation"
+                  type="text"
+                  className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+                />
+              </div>
+
+              <div className="bg-white rounded-lg shadow-lg p-6 w-full max-w-md">
+
+      <div className="mb-4">
+        <label htmlFor="equipmentQuantity" className="block text-gray-700 font-bold mb-2">Quantity</label>
+        <input
+          type="range"
+          id="equipmentQuantity"
+          className="w-full accent-indigo-600"
+          min="0"
+          max="100"
+          value={Qty}
+          onInput={(e) => updateQty(e.target.value)}
+        />
+      </div>
+      <div className="flex justify-between text-gray-500">
+        <span id="minQty">{Qty}</span>
+        <span id="maxQty">100</span>
+      </div>
+    </div>
+ 
+              <div>
+                <label className="block text-sm font-medium text-white">
+                  Image
+                </label>
+                <div className="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
+                  <div className="space-y-1 text-center">
+                    <svg className="mx-auto h-12 w-12 text-white" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
+                      <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                    </svg>
+                    <div className="flex text-sm text-gray-600">
+                      <label htmlFor="file-upload" className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
+                        <span>Upload a file</span>
+                        <input id="file-upload" name="file-upload" type="file" className="sr-only" />
+                      </label>
+                      <p className="pl-1 text-white">or drag and drop</p>
+                    </div>
+                    <p className="text-xs text-white">
+                      PNG, JPG, GIF up to 10MB
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+  
+            <div className="flex justify-end mt-6">
+              <button className="px-6 py-2 leading-5 text-white transition-colors duration-200 transform bg-pink-500 rounded-md hover:bg-pink-700 focus:outline-none focus:bg-gray-600">
+                Submit
+              </button>
+            </div>
+          </form>
+        </section>
+
+      </>
+    );
+}
+
+export default AddEquipment;
