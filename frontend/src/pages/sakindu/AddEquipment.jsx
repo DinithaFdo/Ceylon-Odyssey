ac80d630a88dca9e95ca74c34b9accd0373eff68
Sakindu Dinsara <161465821+Loo0per@users.noreply.github.com>
2024-09-28 17:07:47 +0530

    UI

--- frontend/src/pages/sakindu/AddEquipment.jsx
+++ frontend/src/pages/sakindu/AddEquipment.jsx
@@ -63,119 +63,184 @@ function AddEquipment() {
   }
 
   return (
-    
-    <>
+    <div className="min-h-screen bg-gray-100 py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-3xl mx-auto">
         {successMessage && (
-          <div className="mb-4 p-4 text-white bg-green-600 rounded-md text-center ">
-            {successMessage}
+          <div className="mb-4 p-4 bg-green-100 border-l-4 border-green-500 text-green-700 rounded-md">
+            <p className="font-medium">{successMessage}</p>
           </div>
         )}
-      <section className="max-w-4xl p-6 mx-auto bg-gray-700 rounded-md shadow-md dark:bg-gray-800 mt-20">
-        <h1 className="text-xl font-bold text-white capitalize dark:text-white">Add Equipment</h1>
-
-        
-        
-
-        <form onSubmit={addEquipment}>
-          <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
+  
+        <div className="bg-white shadow-md rounded-lg overflow-hidden">
+          <div className="px-6 py-4 bg-gray-800 border-b border-gray-700">
+            <h2 className="text-2xl font-bold text-white">Add Equipment</h2>
+          </div>
+  
+          <form onSubmit={addEquipment} className="px-6 py-4 space-y-6">
+            <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
             
-
-            <div>
-              <label className="text-white dark:text-gray-200" htmlFor="equipmentId">Equipment ID</label>
-              <input
-                id="equipmentId"
-                type="text"
-                className="block w-full px-4 py-2 mt-2"
-                onChange={(e) => setEquipmentId(e.target.value)}
-              />
-              {errors.equipmentId && <p className="text-red-500">{errors.equipmentId}</p>}
+              <div>
+                <label htmlFor="equipmentId" className="block text-sm font-medium text-gray-700">
+                  Equipment ID
+                </label>
+                <input
+                  id="equipmentId"
+                  type="text"
+                  className="mt-2 block w-full px-4 py-2 border border-gray-500 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-md"
+                  onChange={(e) => setEquipmentId(e.target.value)}
+                />
+                {errors.equipmentId && <p className="mt-2 text-sm text-red-600">{errors.equipmentId}</p>}
+              </div>
+  
+             
+              <div>
+                <label htmlFor="equipmentName" className="block text-sm font-medium text-gray-700">
+                  Equipment Name
+                </label>
+                <input
+                  id="equipmentName"
+                  type="text"
+                  className="mt-2 block w-full px-4 py-2 border border-gray-500 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-md"
+                  onChange={(e) => setEquipmentName(e.target.value)}
+                />
+                {errors.equipmentName && <p className="mt-2 text-sm text-red-600">{errors.equipmentName}</p>}
+              </div>
+  
+              
+              <div>
+                <label htmlFor="equipmentType" className="block text-sm font-medium text-gray-700">
+                  Equipment Type
+                </label>
+                <select
+                  id="equipmentType"
+                  className="mt-2 block w-full px-4 py-2 border border-gray-500 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-md"
+                  onChange={(e) => setEquipmentType(e.target.value)}
+                >
+                  <option value="">Select Type</option>
+                  <option>Hiking</option>
+                  <option>Luggage</option>
+                  <option>Clothes</option>
+                  <option>Toiletries</option>
+                </select>
+                {errors.equipmentType && <p className="mt-2 text-sm text-red-600">{errors.equipmentType}</p>}
+              </div>
+  
+              <div className="sm:col-span-2">
+                <label htmlFor="equipmentDescription" className="block text-sm font-medium text-gray-700">
+                  Equipment Description
+                </label>
+                <textarea
+                  id="equipmentDescription"
+                  rows="4"
+                  className="mt-2 block w-full px-4 py-2 border border-gray-500 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-md"
+                  onChange={(e) => setEquipmentDescription(e.target.value)}
+                />
+                {errors.equipmentDescription && <p className="mt-2 text-sm text-red-600">{errors.equipmentDescription}</p>}
+              </div>
+  
+           
+              <div>
+                <label htmlFor="equipmentPrice" className="block text-sm font-medium text-gray-700">
+                  Equipment Price
+                </label>
+                <div className="mt-2 relative rounded-md shadow-sm">
+                  
+                  <input
+                    id="equipmentPrice"
+                    type="text"
+                    className="block w-full pl-7 pr-12 border border-gray-500 rounded-md focus:ring-indigo-500 focus:border-indigo-500 sm:text-md"
+                    placeholder=" 0.00"
+                    onChange={(e) => setEquipmentPrice(parseFloat(e.target.value))}
+                  />
+                </div>
+                {errors.equipmentPrice && <p className="mt-2 text-sm text-red-600">{errors.equipmentPrice}</p>}
+              </div>
+  
+              
+              <div>
+                <label htmlFor="equipmentQuantity" className="block text-sm font-medium text-gray-700">
+                  Equipment Quantity
+                </label>
+                <input
+                  id="equipmentQuantity"
+                  type="number"
+                  min="0"
+                  className="mt-2 block w-full px-4 py-2 border border-gray-500 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-md"
+                  onChange={(e) => setEquipmentQuantity(parseFloat(e.target.value))}
+                />
+                {errors.equipmentQuantity && <p className="mt-2 text-sm text-red-600">{errors.equipmentQuantity}</p>}
+              </div>
+  
+           
+              <div className="sm:col-span-2">
+                <label htmlFor="equipmentImage" className="block text-sm font-medium text-gray-700">
+                  Upload Image
+                </label>
+                <div className="mt-2 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-500 border-dashed rounded-md">
+                  <div className="space-y-1 text-center">
+                    <svg
+                      className="mx-auto h-12 w-12 text-gray-400"
+                      stroke="currentColor"
+                      fill="none"
+                      viewBox="0 0 48 48"
+                      aria-hidden="true"
+                    >
+                      <path
+                        d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+                        strokeWidth={2}
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                    </svg>
+                    <div className="flex text-sm text-gray-600">
+                      <label
+                        htmlFor="equipmentImage"
+                        className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500"
+                      >
+                        <span>Upload a file</span>
+                        <input
+                          id="equipmentImage"
+                          name="equipmentImage"
+                          type="file"
+                          className="sr-only"
+                          onChange={(e) => {
+                            setEquipmentImage(e.target.files[0]);
+                            const file = e.target.files[0];
+                            if (file) {
+                              const reader = new FileReader();
+                              reader.onload = (event) => {
+                                document.getElementById('imagePreview').src = event.target.result;
+                              };
+                              reader.readAsDataURL(file);
+                            }
+                          }}
+                        />
+                      </label>
+                      
+                    </div>
+                    <p className="text-xs text-gray-500">PNG, JPG, GIF up to 10MB</p>
+                    <img id="imagePreview" alt="Selected Image" className="mx-auto mt-2 max-h-24" />
+                  </div>
+                </div>
+              </div>
             </div>
-
-            <div>
-              <label className="text-white dark:text-gray-200" htmlFor="equipmentName">Equipment Name</label>
-              <input
-                id="equipmentName"
-                type="text"
-                className="block w-full px-4 py-2 mt-2"
-                onChange={(e) => setEquipmentName(e.target.value)}
-              />
-              {errors.equipmentName && <p className="text-red-500">{errors.equipmentName}</p>}
-            </div>
-
-            <div>
-              <label className="text-white dark:text-gray-200" htmlFor="equipmentType">Equipment Type</label>
-              <select
-                id="equipmentType"
-                className="block w-full px-4 py-2 mt-2"
-                onChange={(e) => setEquipmentType(e.target.value)}
+  
+           
+            <div className="flex items-center justify-end">
+              <button
+                type="submit"
+                className="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-md font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
               >
-                <option value="">Select Type</option>
-                <option>Hiking</option>
-                <option>Luggage</option>
-                <option>Clothes</option>
-                <option>Toiletries</option>
-              </select>
-              {errors.equipmentType && <p className="text-red-500">{errors.equipmentType}</p>}
+                Add Equipment
+              </button>
             </div>
-
-            
-            <div>
-              <label className="text-white dark:text-gray-200" htmlFor="equipmentDescription">Equipment Description</label>
-              <textarea
-                id="equipmentDescription"
-                className="block w-full px-4 py-2 mt-2"
-                onChange={(e) => setEquipmentDescription(e.target.value)}
-              />
-              {errors.equipmentDescription && <p className="text-red-500">{errors.equipmentDescription}</p>}
-            </div>
-
-            
-            <div>
-              <label className="text-white dark:text-gray-200" htmlFor="equipmentPrice">Equipment Price</label>
-              <input
-                id="equipmentPrice"
-                type="text"
-                className="block w-full px-4 py-2 mt-2"
-                onChange={(e) => setEquipmentPrice(parseFloat(e.target.value))}
-              />
-              {errors.equipmentPrice && <p className="text-red-500">{errors.equipmentPrice}</p>}
-            </div>
-
-            
-            <div>
-              <label className="text-white dark:text-gray-200" htmlFor="equipmentQuantity">Equipment Quantity</label>
-              <input
-                id="equipmentQuantity"
-                type="text"
-                className="block w-full px-4 py-2 mt-2"
-                onChange={(e) => setEquipmentQuantity(parseFloat(e.target.value))}
-              />
-              {errors.equipmentQuantity && <p className="text-red-500">{errors.equipmentQuantity}</p>}
-            </div>
-            
-            <div>
-              <label className="block mb-2 text-l font-medium text-white dark:text-white" htmlFor="equipmentImage">Upload Image</label>
-              <input
-                name="equipmentImage"
-                className="block w-full p-2.5"
-                id="equipmentImage"
-                type="file"
-                onChange={(e) => {
-                  setEquipmentImage(e.target.files[0]);
-                }}
-              />
-            </div>
-          </div>
-
-          <div className="flex justify-end mt-6">
-            <button type="submit" className="px-6 py-2 leading-5 text-white bg-gray-500 rounded-md hover:bg-green-900">
-              Submit
-            </button>
-          </div>
-        </form>
-      </section>
-    </>
+          </form>
+        </div>
+      </div>
+    </div>
   );
+  
+  
 }
 
 export default AddEquipment;
--- frontend/src/pages/sakindu/Inventory.jsx
+++ frontend/src/pages/sakindu/Inventory.jsx
@@ -58,59 +58,85 @@ function AllEquipment() {
   };
 
   return (
-    <>
-    <div>
-                    <Toaster />
-      <section className="max-w-4xl p-6 mx-auto bg-gray-700 rounded-md shadow-md dark:bg-gray-800 mt-20">
-        
-        <div className="flex justify-between items-center mb-4">
-          <h1 className="text-4xl font-bold text-white capitalize dark:text-white">Inventory</h1>
-          <button
-            onClick={downloadReport}
-            className="bg-blue-900 hover:bg-blue-700 text-white px-2 py-2 rounded"
-          >
-            Download Equipment Report
-          </button>
-        </div>
-        <select
-          value={selectedType}
-          onChange={handleTypeChange}
-          className="mb-4 p-2 rounded border"
-        >
-          <option value="All">All Equipment</option>
-          {equipmentTypes.map((type, index) => (
-            <option key={index} value={type}>{type}</option>
-          ))}
-        </select>
-        <div className="grid grid-cols-1 gap-6 mt-2 sm:grid-cols-3">
-          {filteredEquipment.map((equip) => (
-            <div key={equip._id} className="bg-white dark:bg-gray-800 rounded-md shadow-md">
-              <div className="flex justify-center items-center">
-                <img
-                  src={"http://localhost:5000/EquipmentImages/" + equip.equipmentImage}
-                  alt={equip.equipmentImage}
-                  className="w-auto h-auto sm:h-48 object-cover rounded-t-md"
-                />
-              </div>
-              <div className="p-4">
-                <h2 className="text-xl font-semibold text-gray-800 dark:text-white">{equip.equipmentName}</h2>
-                <p className="text-gray-600 dark:text-gray-300 mt-2">Rs. {equip.equipmentPrice}</p>
-                <p className="text-gray-600 dark:text-gray-300 mt-2">{equip.equipmentType}</p>
-                <p className="text-gray-600 dark:text-gray-300 mt-2">Qty: {equip.equipmentQuantity}</p>
-              </div>
-              <div className="flex justify-end mb-1">
-                <button className="bg-transparent hover:bg-red-500 text-red-700 font-semibold hover:text-white py-2 px-4 border border-red-500 hover:border-transparent rounded mr-3" onClick={() => deleteEquipment(equip._id)}>Delete</button>
-                <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded mr-1">
-                  <Link to={`/Equipment-dashboard/updateequipment/${equip._id}`} state={{ equipmentToEdit: equip }}>Update</Link>
-                </button>
-              </div>
+    <div className="min-h-screen bg-gray-100 py-8 px-4 sm:px-6 lg:px-8">
+      <Toaster />
+      <div className="max-w-7xl mx-auto">
+        <div className="bg-white shadow-lg rounded-lg overflow-hidden">
+          <div className="px-6 py-4 bg-gray-800 sm:px-8">
+            <div className="flex flex-col sm:flex-row justify-between items-center">
+              <h1 className="text-3xl font-bold text-white mb-4 sm:mb-0">Equipment Inventory</h1>
+              <button
+                onClick={downloadReport}
+                className="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded-md transition duration-300 ease-in-out transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
+              >
+                Download Low Stock Report
+              </button>
             </div>
-          ))}
+          </div>
+  
+          <div className="px-6 py-4 border-b border-gray-200 sm:px-8">
+            <select
+              value={selectedType}
+              onChange={handleTypeChange}
+              className="w-full sm:w-auto px-4 py-2 rounded-md border-gray-300 shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
+            >
+              <option value="All">All Equipment</option>
+              {equipmentTypes.map((type, index) => (
+                <option key={index} value={type}>{type}</option>
+              ))}
+            </select>
+          </div>
+  
+          {/* Grid layout for equipment cards */}
+          <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 p-4 sm:p-6">
+            {filteredEquipment.map((equip) => (
+              <div
+                key={equip._id}
+                className="bg-white rounded-lg shadow-md overflow-hidden transition duration-300 ease-in-out transform hover:scale-105 relative"
+              >
+                {/* Reduced image size */}
+                <div className="w-full h-32 flex justify-center items-center p-2">
+                  <img
+                    src={`http://localhost:5000/EquipmentImages/${equip.equipmentImage}`}
+                    alt={equip.equipmentName}
+                    className="object-contain max-h-28"
+                  />
+                </div>
+                <div className="p-3">
+                  <h2 className="text-lg font-semibold text-gray-800 truncate">{equip.equipmentName}</h2>
+                  <p className="text-sm text-gray-600">Type: {equip.equipmentType}</p>
+                  <p className="text-sm text-gray-600">Price: Rs. {equip.equipmentPrice}</p>
+                  <p className="text-sm text-gray-600">Qty: {equip.equipmentQuantity}</p>
+                </div>
+  
+                {/* Spacing between price and buttons */}
+                <div className="p-4"></div>
+  
+                <div className="absolute bottom-0 left-0 right-0 bg-gray-50 flex justify-between p-2">
+                  <button
+                    onClick={() => deleteEquipment(equip._id)}
+                    className="px-2 py-1 bg-red-600 text-white text-xs rounded-md hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-opacity-50 transition duration-300 ease-in-out"
+                  >
+                    Delete
+                  </button>
+                  <Link
+                    to={`/Equipment-dashboard/updateequipment/${equip._id}`}
+                    state={{ equipmentToEdit: equip }}
+                    className="px-2 py-1 bg-blue-600 text-white text-xs rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 transition duration-300 ease-in-out"
+                  >
+                    Update
+                  </Link>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
-      </section>
       </div>
-    </>
+    </div>
   );
+  
+
+
 }
 
 export default AllEquipment;
