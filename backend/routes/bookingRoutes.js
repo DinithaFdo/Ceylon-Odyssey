e2fb0f4e14715192500ccbfb834268edc3f647a5
Dinitha Fernando <info@dinitha.me>
2024-09-29 01:02:17 +0530

    Booking Backend added

--- backend/routes/bookingRoutes.js
+++ backend/routes/bookingRoutes.js
@@ -1,12 +1,11 @@
 const express = require('express');
-const {getAllBookings, createBooking, getBooking, updateBooking, deleteBooking, generateReceipt } = require('../controller/bookingController.js');
+const {getAllBookings, createBooking, getBooking, updateBooking, deleteBooking} = require('../controllers/bookingController');
 const router = express.Router();
 
 router.post('/', createBooking);
 router.get('/:id', getBooking);
 router.put('/:id', updateBooking);
 router.delete('/:id', deleteBooking);
-router.get('/:id/generate-receipt', generateReceipt);
 router.get('/', getAllBookings); 
 
 module.exports = router;
