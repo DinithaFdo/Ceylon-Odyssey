c4e043251a3f997b01d39372c48812bc062548c4
Dinitha Fernando <info@dinitha.me>
2024-09-29 10:51:43 +0530

    Update Signup.jsx

--- frontend/src/pages/signup/Signup.jsx
+++ frontend/src/pages/signup/Signup.jsx
@@ -158,7 +158,7 @@ function SignUp() {
                     onChange={handleChange}
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6 p-5"
                   />
-                  {errors.email && <p className="text-red-600 text-sm mt-1">{errors.email}</p>} {/* Error message */}
+                  {errors.email && <p className="text-red-600 text-sm mt-1">{errors.email}</p>}
                 </div>
               </div>
 
